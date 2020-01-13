91ecdb145fb0fcc4bd412e9926fcd4b34b79dea3
marcos <cosmarcy@gmail.com>
2020-01-13 23:20:13 +0100

    Commit Corrección de una llave de cierre olvidada del último if del onCreate()

--- app/src/main/java/com/elmundobinario/lacomidadelcanco/Almuerzo.java
+++ app/src/main/java/com/elmundobinario/lacomidadelcanco/Almuerzo.java
@@ -79,180 +79,180 @@ public class Almuerzo extends AppCompatActivity {
 
         //Si no es la primera vez, existirá el fichero HORTALIZAS_TUBERCULOS y habrá que cargar el array desde SharedPreferences:
         if (hortalizas_tuberculosSharedPreferences.contains(String.valueOf(0))) {
-            for (int i=0;i<14;i++){
-                hortalizas_tuberculos[i]=hortalizas_tuberculosSharedPreferences.getString(String.valueOf(i),"");
-        } //fin de la carga del array hortalizas_tuberculos
-
-        //Si no es la primera vez, existirá el fichero CONDIMENTOS_HIDRATOS y habrá que cargar el array desde SharedPreferences:
-        if (condimento_hidratosSharedPreferences.contains(String.valueOf(0))) {
-            for (int i=0;i<7;i++){
-                condimento_hidratos[i]=condimento_hidratosSharedPreferences.getString(String.valueOf(i),"");
-        } //fin de la carga del array condimentos_hidratos.
-
-        // primera vez que muestra ingrediente preguntado. Las demás veces en onClicks...
-        if (ordenBloqueAlimento == 1) {
-            preguntaIngrediente.setText(cereales[cerealPreguntado] + "?");
-        }
-
-    }
-
-    public void clickSi(View view) {
-        // 1 = cereales
-        if (ordenBloqueAlimento == 1) {
-            cerealElegido = cereales[cerealPreguntado];
-            RotarArrayAlimento();
-            ordenBloqueAlimento++;
-            preguntaIngrediente.setText(legumbres[legumbrePreguntada] + "?");
-            cerealPreguntado = 0;
-        }
-        // 2 = legumbres:
-        else if (ordenBloqueAlimento == 2) {
-            legumbreElegida = legumbres[legumbrePreguntada];
-            RotarArrayAlimento();
-            ordenBloqueAlimento++;
-            preguntaIngrediente.setText(hortalizas_tuberculos[hortaliza_tuberculoPreguntado] + "?");
-            legumbrePreguntada = 0;
-        }
-        // 3 = hortalizas_tuberculos:
-        else if (ordenBloqueAlimento == 3) {
-            hortalia_tuberculoElegido = hortalizas_tuberculos[hortaliza_tuberculoPreguntado];
-            RotarArrayAlimento();
-            ordenBloqueAlimento++;
-            preguntaIngrediente.setText(condimento_hidratos[condimento_hidratosPreguntado] + "?");
-        }
-        // 4 = condimento_hidratos:
-        else if (ordenBloqueAlimento == 4) {
-            condimento_hidratosElegido = condimento_hidratos[condimento_hidratosPreguntado];
-            RotarArrayAlimento();
-            // Aqui ya se tienen todos los alimentos
-            // AQUI VA EL DÓDIGO PARA GUARDAR DATOS DE ARRAYS EN ARCHIVOS
-            guardarArrayCereales();
-            guardarArrayLegumbres();
-            guardarArrayHortalizaTuberculo();
-            guardarArrayCondimentoHidratos();
-            condimento_hidratosPreguntado = 0;
-
-        }
-    }
-
-    public void clickNo(View view) {
-    }
-
-    private void RotarArrayAlimento() {
-        // ROTAR CEREALES:
-        if (ordenBloqueAlimento == 1) {
-            String reordenandoCereales[] = new String[8];
-            reordenandoCereales[7] = cereales[cerealPreguntado];
-            if (cerealPreguntado == 0) {
-                for (int i = 0; i == 7; i++) {
-                    reordenandoCereales[i] = cereales[i + 1];
-                }
-            } else {
-                for (int i = 0; i < cerealPreguntado; i++) {
-                    reordenandoCereales[i] = cereales[i - 1];
-                }
-                for (int i = cerealPreguntado; i == 7; i++) {
-                    reordenandoCereales[i] = cereales[i + 1];
-                }
-            }
-            for (int i = 0; i < 8; i++) {
-                cereales[i] = reordenandoCereales[i];
-            }
-
-        }
-        //ROTAR LEGUMBRES:
-        else if (ordenBloqueAlimento == 2) {
-            String reordenandoLegumbres[] = new String[9];
-            reordenandoLegumbres[8] = legumbres[legumbrePreguntada];
-            if (legumbrePreguntada == 0) {
-                for (int i = 0; i == 8; i++) {
-                    reordenandoLegumbres[i] = legumbres[i + 1];
-                }
-            } else {
-                for (int i = 0; i < legumbrePreguntada; i++) {
-                    reordenandoLegumbres[i] = legumbres[i - 1];
-                }
-                //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
-                for (int i = legumbrePreguntada; i == 8; i++) {
-                    reordenandoLegumbres[i] = legumbres[i + 1];
-                }
-            }
-            // Pasamos el array de seguridad al array original, actualizando así el array:
-            for (int i = 0; i < 9; i++) {
-                legumbres[i] = reordenandoLegumbres[i];
-            }
-
-        }
-        //ROTAR HORTALIZAS_TUBERCULOS:
-        else if (ordenBloqueAlimento == 3) {
-            String reordenandoHortalizas_Tuberculos[] = new String[14];
-            reordenandoHortalizas_Tuberculos[13] = hortalizas_tuberculos[hortaliza_tuberculoPreguntado];
-            if (hortaliza_tuberculoPreguntado == 0) {
-                for (int i = 0; i == 13; i++) {
-                    reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i + 1];
-                }
-            } else {
-                for (int i = 0; i < hortaliza_tuberculoPreguntado; i++) {
-                    reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i - 1];
-                }
-                //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
-                for (int i = hortaliza_tuberculoPreguntado; i == 13; i++) {
-                    reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i + 1];
-                }
-            }
-            // Pasamos el array de seguridad al array original, actualizando así el array:
             for (int i = 0; i < 14; i++) {
-                hortalizas_tuberculos[i] = reordenandoHortalizas_Tuberculos[i];
-            }
-
+                hortalizas_tuberculos[i] = hortalizas_tuberculosSharedPreferences.getString(String.valueOf(i), "");
+            } //fin de la carga del array hortalizas_tuberculos
         }
-        //ROTAR CONDIMENTO_HIDRATOS:
-        else if (ordenBloqueAlimento == 4) {
-            String reordenandoCondimento_Hidratos[] = new String[7];
-            reordenandoCondimento_Hidratos[6] = condimento_hidratos[condimento_hidratosPreguntado];
-            if (condimento_hidratosPreguntado == 0) {
-                for (int i = 0; i == 6; i++) {
-                    reordenandoCondimento_Hidratos[i] = condimento_hidratos[i + 1];
-                }
-            } else {
-                for (int i = 0; i < condimento_hidratosPreguntado; i++) {
-                    reordenandoCondimento_Hidratos[i] = condimento_hidratos[i - 1];
-                }
-                //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
-                for (int i = condimento_hidratosPreguntado; i == 6; i++) {
-                    reordenandoCondimento_Hidratos[i] = condimento_hidratos[i + 1];
-                }
+            //Si no es la primera vez, existirá el fichero CONDIMENTOS_HIDRATOS y habrá que cargar el array desde SharedPreferences:
+            if (condimento_hidratosSharedPreferences.contains(String.valueOf(0))) {
+                for (int i = 0; i < 7; i++) {
+                    condimento_hidratos[i] = condimento_hidratosSharedPreferences.getString(String.valueOf(i), "");
+                } //fin de la carga del array condimentos_hidratos.
             }
-            // Pasamos el array de seguridad al array original, actualizando así el array:
-            for (int i = 0; i < 7; i++) {
-                condimento_hidratos[i] = reordenandoCondimento_Hidratos[i];
+                // primera vez que muestra ingrediente preguntado. Las demás veces en onClicks...
+                if (ordenBloqueAlimento == 1) {
+                    preguntaIngrediente.setText(cereales[cerealPreguntado] + "?");
+                }
+
             }
 
+            public void clickSi (View view){
+                // 1 = cereales
+                if (ordenBloqueAlimento == 1) {
+                    cerealElegido = cereales[cerealPreguntado];
+                    RotarArrayAlimento();
+                    ordenBloqueAlimento++;
+                    preguntaIngrediente.setText(legumbres[legumbrePreguntada] + "?");
+                    cerealPreguntado = 0;
+                }
+                // 2 = legumbres:
+                else if (ordenBloqueAlimento == 2) {
+                    legumbreElegida = legumbres[legumbrePreguntada];
+                    RotarArrayAlimento();
+                    ordenBloqueAlimento++;
+                    preguntaIngrediente.setText(hortalizas_tuberculos[hortaliza_tuberculoPreguntado] + "?");
+                    legumbrePreguntada = 0;
+                }
+                // 3 = hortalizas_tuberculos:
+                else if (ordenBloqueAlimento == 3) {
+                    hortalia_tuberculoElegido = hortalizas_tuberculos[hortaliza_tuberculoPreguntado];
+                    RotarArrayAlimento();
+                    ordenBloqueAlimento++;
+                    preguntaIngrediente.setText(condimento_hidratos[condimento_hidratosPreguntado] + "?");
+                }
+                // 4 = condimento_hidratos:
+                else if (ordenBloqueAlimento == 4) {
+                    condimento_hidratosElegido = condimento_hidratos[condimento_hidratosPreguntado];
+                    RotarArrayAlimento();
+                    // Aqui ya se tienen todos los alimentos
+                    // AQUI VA EL DÓDIGO PARA GUARDAR DATOS DE ARRAYS EN ARCHIVOS
+                    guardarArrayCereales();
+                    guardarArrayLegumbres();
+                    guardarArrayHortalizaTuberculo();
+                    guardarArrayCondimentoHidratos();
+                    condimento_hidratosPreguntado = 0;
+
+                }
+            }
+
+            public void clickNo (View view){
+            }
+
+            private void RotarArrayAlimento () {
+                // ROTAR CEREALES:
+                if (ordenBloqueAlimento == 1) {
+                    String reordenandoCereales[] = new String[8];
+                    reordenandoCereales[7] = cereales[cerealPreguntado];
+                    if (cerealPreguntado == 0) {
+                        for (int i = 0; i == 7; i++) {
+                            reordenandoCereales[i] = cereales[i + 1];
+                        }
+                    } else {
+                        for (int i = 0; i < cerealPreguntado; i++) {
+                            reordenandoCereales[i] = cereales[i - 1];
+                        }
+                        for (int i = cerealPreguntado; i == 7; i++) {
+                            reordenandoCereales[i] = cereales[i + 1];
+                        }
+                    }
+                    for (int i = 0; i < 8; i++) {
+                        cereales[i] = reordenandoCereales[i];
+                    }
+
+                }
+                //ROTAR LEGUMBRES:
+                else if (ordenBloqueAlimento == 2) {
+                    String reordenandoLegumbres[] = new String[9];
+                    reordenandoLegumbres[8] = legumbres[legumbrePreguntada];
+                    if (legumbrePreguntada == 0) {
+                        for (int i = 0; i == 8; i++) {
+                            reordenandoLegumbres[i] = legumbres[i + 1];
+                        }
+                    } else {
+                        for (int i = 0; i < legumbrePreguntada; i++) {
+                            reordenandoLegumbres[i] = legumbres[i - 1];
+                        }
+                        //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
+                        for (int i = legumbrePreguntada; i == 8; i++) {
+                            reordenandoLegumbres[i] = legumbres[i + 1];
+                        }
+                    }
+                    // Pasamos el array de seguridad al array original, actualizando así el array:
+                    for (int i = 0; i < 9; i++) {
+                        legumbres[i] = reordenandoLegumbres[i];
+                    }
+
+                }
+                //ROTAR HORTALIZAS_TUBERCULOS:
+                else if (ordenBloqueAlimento == 3) {
+                    String reordenandoHortalizas_Tuberculos[] = new String[14];
+                    reordenandoHortalizas_Tuberculos[13] = hortalizas_tuberculos[hortaliza_tuberculoPreguntado];
+                    if (hortaliza_tuberculoPreguntado == 0) {
+                        for (int i = 0; i == 13; i++) {
+                            reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i + 1];
+                        }
+                    } else {
+                        for (int i = 0; i < hortaliza_tuberculoPreguntado; i++) {
+                            reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i - 1];
+                        }
+                        //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
+                        for (int i = hortaliza_tuberculoPreguntado; i == 13; i++) {
+                            reordenandoHortalizas_Tuberculos[i] = hortalizas_tuberculos[i + 1];
+                        }
+                    }
+                    // Pasamos el array de seguridad al array original, actualizando así el array:
+                    for (int i = 0; i < 14; i++) {
+                        hortalizas_tuberculos[i] = reordenandoHortalizas_Tuberculos[i];
+                    }
+
+                }
+                //ROTAR CONDIMENTO_HIDRATOS:
+                else if (ordenBloqueAlimento == 4) {
+                    String reordenandoCondimento_Hidratos[] = new String[7];
+                    reordenandoCondimento_Hidratos[6] = condimento_hidratos[condimento_hidratosPreguntado];
+                    if (condimento_hidratosPreguntado == 0) {
+                        for (int i = 0; i == 6; i++) {
+                            reordenandoCondimento_Hidratos[i] = condimento_hidratos[i + 1];
+                        }
+                    } else {
+                        for (int i = 0; i < condimento_hidratosPreguntado; i++) {
+                            reordenandoCondimento_Hidratos[i] = condimento_hidratos[i - 1];
+                        }
+                        //aqui la 'i' llega hasta un indice menos del total, donde está la elegida en reordenando:
+                        for (int i = condimento_hidratosPreguntado; i == 6; i++) {
+                            reordenandoCondimento_Hidratos[i] = condimento_hidratos[i + 1];
+                        }
+                    }
+                    // Pasamos el array de seguridad al array original, actualizando así el array:
+                    for (int i = 0; i < 7; i++) {
+                        condimento_hidratos[i] = reordenandoCondimento_Hidratos[i];
+                    }
+
+                }
+
+            }
+
+            private void guardarArrayCereales () {
+                try {
+                    if (!cereales_txt.exists()) {
+                        cereales_txt.createNewFile();
+                    }
+                    PrintWriter bw = new PrintWriter(new BufferedWriter(new FileWriter("cereales.txt")));
+                    for (int i = 0; i < 8; i++) {
+                        bw.write(cereales[i]);
+                    }
+                    bw.close();
+                } catch (IOException e) {
+                    Log.e("Ficheros", "Error al escribir fichero a memoria interna");
+                }
+            }
+
+            private void guardarArrayLegumbres () {
+            }
+
+            private void guardarArrayHortalizaTuberculo () {
+            }
+
+            private void guardarArrayCondimentoHidratos () {
+            }
         }
-
-    }
-
-    private void guardarArrayCereales() {
-        try {
-            if (!cereales_txt.exists()) {
-                cereales_txt.createNewFile();
-            }
-            PrintWriter bw = new PrintWriter(new BufferedWriter(new FileWriter("cereales.txt")));
-            for (int i = 0; i < 8; i++) {
-                bw.write(cereales[i]);
-            }
-            bw.close();
-        } catch (IOException e) {
-            Log.e("Ficheros", "Error al escribir fichero a memoria interna");
-        }
-    }
-
-    private void guardarArrayLegumbres() {
-    }
-
-    private void guardarArrayHortalizaTuberculo() {
-    }
-
-    private void guardarArrayCondimentoHidratos() {
-    }
-}
